d090c9324917172dcad92584ebca86cbd1081107
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-08-06 22:23:40 +0100

    add more files to talent page

---
+++ talent.js
@@ -0,0 +1,6 @@
+const talentBtn = document.querySelector(".hire-link");
+const recuritBtn = document.querySelector(".recurit-div");
+
+talentBtn.addEventListener("click", () => {
+  recuritBtn.classList.toggle("show-div");
+});
